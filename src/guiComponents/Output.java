bbe232fc1ed6554ca8c09a8bb84117c036820a55
David Johnston <healthytechfun@gmail.com>
2012-12-18 13:59:56 +1300

    Refactored output to make a seperate 'drawWaveForm' function.
    put x1, x2, y1, y2 as class scope private variabls.
    created empty drawEnvelope method.

--- src/guiComponents/Output.java
+++ src/guiComponents/Output.java
@@ -32,26 +32,40 @@ public class Output extends JPanel{
 		this.repaint(); 
 	}
 	
+	
+	/*ints used for drawing the waveform*/
+	private int x1; 
+	private int x2; 
+	private int y1; 
+	private int y2;
+	
+	private void drawWaveForm(Graphics2D g, int i)
+	{
+    	/*find where the amplitude is at for the given horizontal pixel*/
+    	x2 = i; 
+    	y2 = (int)(Gui.OUTPUT_HEIGHT*0.5*engine.getValue(i)*-1+Gui.OUTPUT_HEIGHT/2); 
+    	
+    	/*draw a line between current and previous points*/           	
+    	g.drawLine(x1,y1, x2,y2); 
+    	x1 = x2;
+    	y1 = y2; 	
+    	
+		
+		
+	}
+	
 	public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2d = (Graphics2D) g;     
             
-            int x1 = 0; 
-            int y1 = (int)engine.getValue(0); 
+             x1 = 0; 
+             y1 = (int)engine.getValue(0); 
             
             /*draw the wave*/
             for (int i = 0; i<Gui.OUTPUT_WIDTH; i++)
             {
-            	/*find where the amplitude is at for the given horizontal pixel*/
-            	int x2 = i; 
-            	int y2 = (int)(Gui.OUTPUT_HEIGHT*0.5*engine.getValue(i)*-1+Gui.OUTPUT_HEIGHT/2); 
-            	
-            	/*draw a line between current and previous points*/           	
-            	g2d.drawLine(x1,y1, x2,y2); 
-            	x1 = x2;
-            	y1 = y2; 	
-            	
+            	drawWaveForm(g2d, i); 
             }
        
     }
