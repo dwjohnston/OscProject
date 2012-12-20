12a4ba2d5625b3c6060d84cbd133c135b2c1c0c3
David Johnston <healthytechfun@gmail.com>
2012-12-20 14:48:51 +1300

    Added smoothing function to envelope follower.

--- src/dspComponents/EnvelopeFollower.java
+++ src/dspComponents/EnvelopeFollower.java
@@ -8,12 +8,19 @@ import org.apache.commons.collections.buffer.CircularFifoBuffer;
 
 public class EnvelopeFollower {
 	
-	final int bufferSize = 20; 
-	Buffer buffer; 
+	final int BUFFER_SIZE = 200;
+	final double SMOOTHING_EXPONENT = 1.01;
+	final double SMOOTHING_MULTIPLIER = 0.99; 
+	
+	
+	
+	Buffer buffer;
+	Buffer maxBuffer; 
 	
 	public void reset()
 	{
-		buffer = new CircularFifoBuffer(bufferSize);
+		buffer = new CircularFifoBuffer(BUFFER_SIZE);
+		maxBuffer = new CircularFifoBuffer(BUFFER_SIZE);
 	}
 	
 	public EnvelopeFollower()
@@ -24,7 +31,7 @@ public class EnvelopeFollower {
 	public double getValue(double t)
 	{
 		buffer.add(Math.abs(t)); 
-		
+				
 		Iterator<Double> itr = buffer.iterator();		
 		double max = 0; 
 		while (itr.hasNext())
@@ -32,11 +39,24 @@ public class EnvelopeFollower {
 			double value = itr.next(); 
 			
 			if (value> max) max = value; 
+			else max = Math.pow((max*SMOOTHING_MULTIPLIER),  SMOOTHING_EXPONENT); 
 		
 		}
-
-		return max; 
+//		maxBuffer.add(max); 
+//		Iterator<Double> maxItr = maxBuffer.iterator();
+//		double sum = 0; 
+//		while (maxItr.hasNext())
+//		{
+//			double vmax = maxItr.next();
+//			
+//			sum += vmax *vmax; 
+//		}
+//		sum = sum/ maxBuffer.size(); 
+//		sum = Math.sqrt(sum); 
+//		
+//		return sum; 
 		
+		return max; 
 	}
 	
 	
